be80665fe10cc57fcb96e2ae063080762ee6dcda
ghanwaasim <ghanwa.scp@gmail.com>
2020-07-24 15:29:21 +0500

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -32,6 +34,7 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddCors();
+            services.AddMediatR(typeof(List.Handler).Assembly);
             /*opt =>
             {
                 opt.AddPolicy("CorsPolicy", policy=>
